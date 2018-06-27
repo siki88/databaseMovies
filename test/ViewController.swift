88955dd714d5062a6bec31816b42fff4f41f952c
Lukas Spurny <lukasspurny@macpc1s-mac-mini.local>
2018-06-27 07:36:12 +0200

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Lukas Spurny on 27.06.18.
+//  Copyright © 2018 spurny.lukas@post.cz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
